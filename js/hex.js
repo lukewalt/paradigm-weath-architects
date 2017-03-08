9ba7ed10806bd59716d7d6a5e3bc08db2d18bbeb
Luke Woodward <luke.wdwrd@gmail.com>
2017-03-08 13:29:06 -0600

    added mobile responsive features

--- js/hex.js
+++ js/hex.js
@@ -7,7 +7,7 @@ let height = window.innerHeight;
 
 let centerX = width * 0.5;
 let centerY = height * 0.5;
-let colors = ["#1d5672", "#424254", "lightblue", "#E8CAA4", "tomato"];
+let colors = ["#1d5672", "#444", "lightblue", "#E8CAA1", "tomato"];
 
 canvas.width = width;
 canvas.height = height;
--- js/main.js
+++
@@ -1,5 +0,0 @@
-console.log("welcome to the website");
-
-$(document).ready(function(){
- $('.parallax').parallax();
-});
