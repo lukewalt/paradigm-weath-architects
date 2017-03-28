7ee3d6016368a47256aaea262ad97e9f83f47bf1
Luke Woodward <luke.wdwrd@gmail.com>
2017-03-27 22:50:35 -0500

    cont

---
+++ controllers/main.js
@@ -0,0 +1,13 @@
+app.controller('MainCtrl', function ($scope, hexFactory){
+
+  (function($){
+    $(function(){
+
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+
+
+})
