a07c6d865c1e960f609bfb8cccbe04f734cec668
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-03 21:43:31 -0500

    interactive nav started

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,29 @@
-app.controller('MainCtrl', function ($scope, hexFactory){
+app.controller('MainCtrl', function ($scope){
 
-  (function($){
-    $(function(){
+  // (function($){
+  //   $(function(){
+  //
+  //     $('.button-collapse').sideNav();
+  //     $('.parallax').parallax();
+  //
+  //   }); // end of document ready
+  // })(jQuery); // end of jQuery name space
 
-      $('.button-collapse').sideNav();
-      $('.parallax').parallax();
+  var menu = document.querySelector(".main-nav"),
+      toggle = document.querySelector(".nav-toggle"),
+      body = document.getElementsByTagName('body')[0];
 
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
+  function toggleToggle() {
+    toggle.classList.toggle("menu-open");
+  };
+
+  function toggleMenu() {
+    menu.classList.toggle("active");
+    body.classList.toggle("is-active-menu");
+  };
+
+  toggle.addEventListener("click", toggleToggle, false);
+  toggle.addEventListener("click", toggleMenu, false);
 
 
 })
