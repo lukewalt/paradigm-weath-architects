d94f42fe12a3509e12732ba89b8350ab2c288512
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-10 15:42:56 -0500

    set prefix to null

--- init.js
+++ init.js
@@ -2,7 +2,7 @@ var app = angular.module('pwa', ['ngRoute']);
 
 app.config(($routeProvider, $locationProvider) => {
 
-  $locationProvider.hashPrefix('!');
+  $locationProvider.hashPrefix('');
   console.log('Config executing')
 
   // FIREBASE AUTH
@@ -16,44 +16,44 @@ app.config(($routeProvider, $locationProvider) => {
 
 
   $routeProvider
-    .when('/login', {
+    .when('/', {
         controller: 'LoginCtrl',
-        templateUrl: 'partials/login.html'
+        templateUrl: '/partials/login.html'
     })
     .when('/home', {
         controller: 'MainCtrl',
-        templateUrl: 'partials/main.html'
+        templateUrl: '/partials/main.html'
     })
     .when('/why', {
         controller: 'WhyCtrl',
-        templateUrl: 'partials/why.html'
+        templateUrl: '/partials/why.html'
     })
     .when('/how', {
         controller: 'HowCtrl',
-        templateUrl: 'partials/how.html'
+        templateUrl: '/partials/how.html'
     })
     .when('/who', {
         controller: 'WhoCtrl',
-        templateUrl: 'partials/who.html'
+        templateUrl: '/partials/who.html'
     })
     .when('/mission', {
         controller: 'MisVisCtrl',
-        templateUrl: 'partials/mission.html'
+        templateUrl: '/partials/mission.html'
     })
     .when('/vision', {
         controller: 'MisVisCtrl',
-        templateUrl: 'partials/vision.html'
+        templateUrl: '/partials/vision.html'
     })
     .when('/philosophy', {
         controller: 'MisVisCtrl',
-        templateUrl: 'partials/philosophy.html'
+        templateUrl: '/partials/philosophy.html'
     })
     .when('/resources', {
         controller: 'ResrCtrl',
-        templateUrl: 'partials/resources.html'
+        templateUrl: '/partials/resources.html'
     })
     .otherwise({
-        redirectTo: '/login'
+        redirectTo: '/'
     })
 
 })
