485088729b99a8ff3425e74f5e097363f2b5ef48
Luke Woodward <luke.wdwrd@gmail.com>
2017-04-14 15:40:56 -0500

    imported new cont into who

--- controllers/who.js
+++ controllers/who.js
@@ -1,6 +1,11 @@
 app.controller('WhoCtrl', function($scope, $location ,authFactory){
   console.log('Who');
 
+  AOS.init({
+    duration: 1100
+  })
+
+
   $scope.doLogout = () => {
     authFactory.logout()
     .then(() => $location.path('/'))
