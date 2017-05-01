bf398a708c43885b0e2bf0f6a8bc112bd09b2470
Luke Woodward <luke.wdwrd@gmail.com>
2017-05-01 00:44:18 -0500

    added revolving subheader to banner

--- controllers/main.js
+++ controllers/main.js
@@ -23,5 +23,26 @@ app.controller('MainCtrl', function ($scope, $location, authFactory){
   $('.main-header').height(vph)
 
 
+  let duration = 2500;  // change this to change rotation time in milliseconds
+  let current = 1;
+  let tricker = $(".tricker");
+  let height = tricker.height();
+  let number = tricker.children().length;
+  let first = tricker.children().first();
+
+  const banner = setInterval(function() {
+    let interv = current * -1 * height;
+    first.css("margin-top", interv + "px");
+    if (current == number) {
+      first.css("margin-top", "0px");
+      current = 1;
+    } else {
+      current++;
+    }
+  }, duration);
+
+  $('document').on('load', banner)
+
+
 
 })
